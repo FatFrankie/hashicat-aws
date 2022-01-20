f1288e56abc1223a0443835d3bf53959889f06cb
Frankie <29932243+FatFrankie@users.noreply.github.com>
2022-01-20 12:01:59 +0000

    Added the first tag again

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ terraform {
 
 provider "aws" {
   region  = var.region
-  Department = devops
 }
 
 resource "aws_vpc" "hashicat" {
@@ -126,6 +125,8 @@ resource "aws_instance" "hashicat" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
+  Department = devops
+
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
