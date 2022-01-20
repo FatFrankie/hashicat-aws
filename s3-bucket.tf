7dfe2fec0158e7c784447363e7a53f47491437af
Frankie <29932243+FatFrankie@users.noreply.github.com>
2022-01-20 12:57:00 +0000

    Added S3 Bucket with version - reset prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,8 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.2.0"
-
-  bucket = "fra_frankieWasHere-s3-bucket"
+  bucket_prefix = "fra"
+  bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
